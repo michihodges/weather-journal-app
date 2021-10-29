17ffd020987916285693295f66ad51d0b6a8d733
michihodges <michihodges@gmail.com>
2021-10-29 23:54:07 +0200

    feat: Express Setup

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+
+// Require Body-Parser and CORS
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
