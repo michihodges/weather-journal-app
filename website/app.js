5cdb7db538efe0d86c533f00ffce53d5786f3c27
michihodges <michihodges@gmail.com>
2021-11-08 23:18:55 +0100

    feat: API Key
    API Key and Bas URL added as global variables.

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
+const apiKey = '83fe249da30258ba3370fbbc31584eff';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
