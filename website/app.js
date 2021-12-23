526de8129dc5653313ec3fa2d609a8ac093ee68a
Michael Hodges <michihodges@gmail.com>
2021-12-23 20:58:08 +0100

    feat: Async GET
    Added all data required for API call.

--- website/app.js
+++ website/app.js
@@ -1,17 +1,21 @@
 /* Global Variables */
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = '83fe249da30258ba3370fbbc31584eff&units=imperial';
+const apiKey = '83fe249da30258ba3370fbbc31584eff';
+const testZip = '10001'; // zip code for testing only
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Async GET
-const retrieveData = async (baseUrl, zip, apiKey) =>{ 
-  const request = await fetch(url);
+const retrieveData = async (baseUrl, testZip, apiKey) =>{ 
+  const res = await fetch(baseUrl + testZip + '&appid=' + apiKey + '&units=imperial');
+  console.log(res);
   try {
   // Transform into JSON
-  const allData = await request.json()
+    const data = await res.json();
+    console.log(data);
+    return data;
   }
   catch(error) {
     console.log("error", error);
@@ -19,12 +23,15 @@ const retrieveData = async (baseUrl, zip, apiKey) =>{
   }
 }
 
+
+// TEST CODE
 // GET request
 const getData = async ( url = '')=>{
   const res = await fetch (url);
   try{
     const data = await res.json();
     console.log(data);
+    return data;
   }catch(error){
     console.log("error",error);
   }
