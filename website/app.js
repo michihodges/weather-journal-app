6dbbcb7211c42a317302ad1e04f50c227d10f7f0
Michael Hodges <michihodges@gmail.com>
2022-01-17 22:46:30 +0100

    feat: Event Listener
    Created event listener with click and callBack parameter.

--- website/app.js
+++ website/app.js
@@ -3,9 +3,14 @@ const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '83fe249da30258ba3370fbbc31584eff';
 const testZip = '10001'; // zip code for testing only
 
+const btn = document.querySelector('#generate');
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event Listener
+btn.addEventListener('click' , callBack);
 
 // Async GET
 const retrieveData = async (baseUrl, testZip, apiKey) =>{ 
