b054edfbff91f5cb424a80bc43973dd9d3037ee2
Michael Hodges <michihodges@gmail.com>
2022-01-19 09:37:26 +0100

    feat: Console Logs
    Added console logs to see what the browser printed.

--- website/app.js
+++ website/app.js
@@ -3,22 +3,24 @@ const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '83fe249da30258ba3370fbbc31584eff';
 const testZip = '10001'; // zip code for testing only
 
-const btn = document.querySelector('#generate');
+// const btn = document.querySelector('#generate');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Event Listener with callBack
-btn.addEventListener('click' , callBack);
+// btn.addEventListener('click' , callBack);
 
 // Async GET
 const retrieveData = async (baseUrl, testZip, apiKey) =>{ 
   const res = await fetch(baseUrl + testZip + '&appid=' + apiKey + '&units=imperial');
+  console.log('response');
   console.log(res);
   try {
   // Transform into JSON
     const data = await res.json();
+    console.log('data');
     console.log(data);
     return data;
   }
